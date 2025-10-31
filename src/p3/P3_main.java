2306e887acf5dfed45442924353fbef80ef6b1f0
esmanur73 <esaglam@smail.uni-koeln.de>
2025-10-31 13:15:45 +0100

    init commit

--- src/p3/P3_main.java
+++ src/p3/P3_main.java
@@ -4,7 +4,7 @@ public class P3_main {
     public static void main(String[] args) {
         int i = 0;
         int j = 0;
-        int k = 0;
+        int k = 1;
 
         if (i < j) {
         System.out.println("A");
