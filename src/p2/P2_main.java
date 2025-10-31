7a06f0508bd900baba54cb612e9fdd6bf2dbea2f
esmanur73 <esaglam@smail.uni-koeln.de>
2025-10-31 12:44:37 +0100

    init commit

---
+++ src/p2/P2_main.java
@@ -0,0 +1,15 @@
+package p2;
+
+public class P2_main {
+    public static void main(String[] args) {
+        int i = 5;
+        boolean b = false;
+
+        if (i > 10) {
+            System.out.println(b = true);
+
+        } else {
+            System.out.println(b = false);
+        }
+    }
+}
